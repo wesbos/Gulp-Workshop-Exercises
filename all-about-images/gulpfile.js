6bd6a76c6b969e380ab0cc1b5671b87f2e22f161
wesbos <wesbos@gmail.com>
2015-11-11 15:22:42 -0500

    fixed #10

--- all-about-images/gulpfile.js
+++ all-about-images/gulpfile.js
@@ -5,19 +5,7 @@ var p = require('gulp-load-plugins')();
 var imageminMozjpeg = require('imagemin-mozjpeg');
 
 gulp.task('images', function() {
-  return gulp.src('source-images/*')
-    .pipe(p.imagemin({
-      optimizationLevel: 7,
-      progressive: true,
-      interlaced: true,
-      multipass: true,
-      svgoPlugins: [{removeViewBox: true}],
-      use: [imageminMozjpeg()]
-    }))
-    // .pipe(p.imagemin({
-    //   use: [imageminMozjpeg()]
-    // }))
-    .pipe(gulp.dest('compressed-images/'));
+  
 });
 
 var del = require('del');
